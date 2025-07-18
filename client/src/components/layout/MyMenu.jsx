bca7cd1bc949712e7387bb25113ef8e8436e7e80
Iury Roque <iury.roque@hotmail.com>
2025-07-18 19:45:13 -0300

    fix(menu): Remove itens do menu deixando somente os iniciais

--- client/src/components/layout/MyMenu.jsx
+++ client/src/components/layout/MyMenu.jsx
@@ -1,18 +1,12 @@
 import {
-    Book,
-    CheckCircle,
-    Event,
-    ExpandLess,
-    ExpandMore,
-    Home,
-    LibraryBooks,
-    Notifications,
-    People,
-    School,
-    TaskAlt,
-    ViewModule
+  CheckCircle,
+  Event,
+  Home,
+  Notifications,
+  People,
+  ViewModule
 } from '@mui/icons-material';
-import { Collapse, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Collapse, List } from '@mui/material';
 import React, { useState } from 'react';
 import { DashboardMenuItem, MenuItemLink, useSidebarState } from 'react-admin';
 
@@ -32,43 +26,43 @@ const MyMenu = ({ onMenuClick }) => {
 
       {/* Itens de menu que continuarão no nível principal */}
       <MenuItemLink to="/pessoa" primaryText="Pessoas" leftIcon={<People />} onClick={onMenuClick} sidebarIsOpen={open} />
-      <MenuItemLink to="/pessoa/cards" primaryText="Acompanhamento de Visitas" leftIcon={<ViewModule />} onClick={onMenuClick} sidebarIsOpen={open} />
+      <MenuItemLink to="/pessoa/cards" primaryText="Visitas" leftIcon={<ViewModule />} onClick={onMenuClick} sidebarIsOpen={open} />
       <MenuItemLink to="/presenca" primaryText="Controle de Presença" leftIcon={<CheckCircle />} onClick={onMenuClick} sidebarIsOpen={open} />
       <MenuItemLink to="/culto" primaryText="Cultos e Eventos" leftIcon={<Event />} onClick={onMenuClick} sidebarIsOpen={open} />
-      <MenuItemLink to="/acompanhamentoEstudo" primaryText="Acompanhamento de Estudos" leftIcon={<School />} onClick={onMenuClick} sidebarIsOpen={open} />
+      {/* <MenuItemLink to="/acompanhamentoEstudo" primaryText="Acompanhamento de Estudos" leftIcon={<School />} onClick={onMenuClick} sidebarIsOpen={open} /> */}
       
       {/* --- Início do Submenu de Currículo --- */}
-      <ListItem button onClick={handleCurriculoClick}>
+      {/* <ListItem button onClick={handleCurriculoClick}>
         <ListItemIcon><LibraryBooks /></ListItemIcon>
         <ListItemText primary="Gerenciar Currículo" />
         {isCurriculoOpen ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
+      </ListItem> */}
       <Collapse in={isCurriculoOpen} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           {/* Item "Estudos" dentro do submenu */}
-          <MenuItemLink
+          {/* <MenuItemLink
             to="/curriculoEstudo"
             primaryText="Estudos"
             leftIcon={<LibraryBooks />}
             onClick={onMenuClick}
             sidebarIsOpen={open}
             sx={{ pl: 4 }} // Padding para identação
-          />
+          /> */}
           {/* Item "Lições" dentro do submenu */}
-          <MenuItemLink
+          {/* <MenuItemLink
             to="/licoesCurriculo"
             primaryText="Lições"
             leftIcon={<Book />}
             onClick={onMenuClick}
             sidebarIsOpen={open}
             sx={{ pl: 4 }} // Padding para identação
-          />
+          /> */}
         </List>
       </Collapse>
       {/* --- Fim do Submenu de Currículo --- */}
 
       {/* Restante dos itens de menu */}
-      <MenuItemLink to="/licoesConcluidasPessoa" primaryText="Lições Concluídas" leftIcon={<TaskAlt />} onClick={onMenuClick} sidebarIsOpen={open} />
+      {/* <MenuItemLink to="/licoesConcluidasPessoa" primaryText="Lições Concluídas" leftIcon={<TaskAlt />} onClick={onMenuClick} sidebarIsOpen={open} /> */}
       <MenuItemLink to="/alertas" primaryText="Alertas e Notificações" leftIcon={<Notifications />} onClick={onMenuClick} sidebarIsOpen={open} />
     </List>
   );
